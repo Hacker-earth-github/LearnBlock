fb7ec82e17a2675511f154bdaaab669522308d0c
anaboy01 <aliyuanate016@gmail.com>
2025-07-22 22:41:18 +0100

    contract deployed

--- contract/ignition/modules/EduTechQuiz.ts
+++ contract/ignition/modules/EduTechQuiz.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const EduTechQuizModule = buildModule("EduTechQuizModule", (m) => {
  
-  const trustees =["0x577eE4c12F0943DB8E7E08D324db068d03EE0B36"]
+  const trustees =["", "", "", "",]
  
 
 
